eb5f35e32cb145f4889411d58c405cad091e6aa7
ArcOmotola <ogunsola40@gmail.com>
2021-11-17 21:37:20 +0100

    added admin dashboard

--- client/src/redux/userSlice.js
+++ client/src/redux/userSlice.js
@@ -13,7 +13,7 @@ const userSlice = createSlice({
         },
         loginSuccess: (state, action) => {
             state.isFetching = false;
-            state.currentUser = action.payload;
+            state.currentUser = action.payload;                             //returns string/object from mongodb of currentUser information (id, username, email, isAdmin, createdAt....)
         },
         loginFailure: (state) => {
             state.isFetching = false;
--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxN2YwMzFmY2ZlODFmYzBmNWVmZWVmMSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNjY3Mjk2MiwiZXhwIjoxNjM2OTMyMTYyfQ.1aUqb0rROavKaVxyewoxgDlrY19hZh52E7LTkC5rOBA";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
