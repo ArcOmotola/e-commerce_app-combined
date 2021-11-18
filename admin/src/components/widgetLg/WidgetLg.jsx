579f3c823a592aa6da043e222304384ceb50b3fe
ArcOmotola <ogunsola40@gmail.com>
2021-11-18 15:56:13 +0100

    fetched dashboard chart data from api

--- admin/src/components/widgetLg/WidgetLg.jsx
+++ admin/src/components/widgetLg/WidgetLg.jsx
@@ -32,7 +32,7 @@ export const WidgetLg = () => {
                     <th className="widgetLgTh">Status</th>
                 </tr>
                 {orders.map(order => (
-                    <tr className="widgetLgTr">
+                    <tr className="widgetLgTr" key={order._id}>
                         <td className="widgetLgUser">
                             <span className="widgetLgName">{order.userId}</span>
                         </td>
--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,16 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Chart } from '../../components/chart/Chart';
 import { FeaturedInfo } from '../../components/featuredInfo/FeaturedInfo';
 import "./home.css";
-import { userData } from '../../dummyData';
+// import { userData } from '../../dummyData';
 import { WidgetSm } from '../../components/widgetSm/WidgetSm';
 import { WidgetLg } from '../../components/widgetLg/WidgetLg';
+import { useMemo, useState } from "react";
+import { userRequest } from '../../requestMethods';
 
 export const Home = () => {
+    const [ userStats, setUserStats ] = useState([]);
+
+    const MONTHS = useMemo(
+        () => [
+            "Jan",
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "Jun",
+            "Jul",
+            "Aug",
+            "Sep",
+            "Oct",
+            "Nov",
+            "Dec",
+        ],
+        []
+    );
+
+    useEffect(() => {
+        const getStats = async () => {
+            try {
+                const res = await userRequest.get("/users/stats");
+                res.data.map((item) => 
+                    setUserStats((prev) => [
+                        ...prev,
+                        { name: MONTHS[item._id - 1], "Active User": item.total },
+                    ])
+                );
+            } catch {}
+        };
+        getStats();
+    }, [MONTHS]);
+
+    console.log(userStats);
+
     return (
         <div className="home">
             <FeaturedInfo />
-            <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+            <Chart data={userStats} title="User Analytics" grid dataKey="Active User"/>
             <div className="homeWidgets">
                 <WidgetSm />
                 <WidgetLg />
